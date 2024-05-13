b345ddc53ee0b02240955d6587e67af7c8af83c7
Arvindh <arvindh@innotechlabs.com.my>
2024-05-13 17:33:04 +0800

    Added-Restricted-Display-To-Notification

--- iTEMS/obj/Debug/net8.0/iTEMS.AssemblyInfo.cs
+++ iTEMS/obj/Debug/net8.0/iTEMS.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iTEMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a646299ea60b9cd5ce049f608c3cc6c3e8873ca9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baabef3cc9b91d9af3778546118368037ad39a6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("iTEMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iTEMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
