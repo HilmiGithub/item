9eaa18167fb01d460602229ff9d75e6bb67bf342
Arvindh <arvindh@innotechlabs.com.my>
2024-05-14 12:46:17 +0800

    Notification feature has been completed. Partially completed dashboard

---
+++ iTEMS/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,31 @@
+﻿using iTEMS.Models;
+
+namespace iTEMS.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int TotalTasks { get; set; }
+        public int PlanningTasks { get; set; }
+        public int PendingTasks { get; set; }
+        public int DelayedTasks { get; set; }
+        public int BlockedTasks { get; set; }
+        public int CompletedTasks { get; set; }
+        public int TotalProjects { get; set; }
+        public int PlanningProjects { get; set; }
+        public int PendingProjects { get; set; }
+        public int DelayedProjects { get; set; }
+        public int BlockedProjects { get; set; }
+        public int CompletedProjects { get; set; }
+
+        public List<Project> ActiveProjectsList { get; set; }
+        public List<TaskTracker> ActiveTasksList { get; set; }
+
+        public List<Employee> TeamMembers { get; set; }
+        public Dictionary<Employee, List<Project>> TeamMembersProjects { get; set; }
+        public Dictionary<Employee, List<TaskTracker>> TeamMembersTasks { get; set; }
+
+
+        // Additional properties or collections as needed
+    }
+
+}
